0d2b7b94413dfc0f1d4489eefc7d14bed5c71180
Kyle Decot <kyle.decot@me.com>
2013-07-08 12:46:43 -0400

    Implements `get`

--- lib/opic.rb
+++ lib/opic.rb
@@ -1,5 +1,16 @@
 require "opic/version"
+require "pry"
 
 module Opic
-  # Your code goes here...
+  ENDPOINT = "https://opic.osu.edu/"
+
+  def self.get(name_n, options = {})
+    defaults = { width: 100, aspect: 's' }
+    options = defaults.merge(options)
+    "#{ENDPOINT}#{name_n}?width=#{options[:width]}&aspect=#{options[:aspect]}"
+  end
+
+  def self.post(name_n, &block)
+    # TODO...
+  end
 end
