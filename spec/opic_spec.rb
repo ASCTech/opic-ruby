83e3c9634590e2c4cf98dc80c6f1e3e8474b202e
Kyle Decot <kyle.decot@me.com>
2013-07-08 14:15:36 -0400

    Improving w/ contexts

--- spec/opic_spec.rb
+++ spec/opic_spec.rb
@@ -3,12 +3,15 @@ require "opic"
 
 describe Opic do
 
-  it 'raises an exception when no API key is present' do
-    expect(Opic.get('foobar')).to raise_error
+  context 'API Key' do
+    it 'raises an exception when not present' do
+      expect(Opic.get('foobar')).to raise_error
+    end
+
+    it 'does not complain when present' do
+      Opic.api_key = '1234'
+      Opic.get('foobar')
+    end
   end
 
-  it 'does not complain when an API key is present' do
-    Opic.api_key = '1234'
-    Opic.get('foobar')
-  end
 end
