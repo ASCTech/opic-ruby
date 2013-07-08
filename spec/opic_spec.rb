e194fe5d281d32de0245a65d905d9e25180ad03d
Kyle Decot <kyle.decot@me.com>
2013-07-08 13:45:14 -0400

    Adds specs to ensure that api key is set

--- spec/opic_spec.rb
+++ spec/opic_spec.rb
@@ -3,4 +3,12 @@ require "opic"
 
 describe Opic do
 
+  it 'raises an exception when no API key is present' do
+    expect(Opic.get('foobar')).to raise_error
+  end
+
+  it 'does not complain when an API key is present' do
+    Opic.api_key = '1234'
+    Opic.get('foobar')
+  end
 end
